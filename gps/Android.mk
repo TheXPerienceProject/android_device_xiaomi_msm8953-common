be0ef4d3884bccc074ddc6ecc9917ed28f3584db
klozz <klozz707@gmail.com>
2018-04-06 07:05:00 -0400

    msm8953-common:Update GPS HIDL HAL from upstream  * Tag: LA.UM.6.6.r1-06700-89xx.0
    Change-Id: I03a98ac008c6861e12bccc4dbefbafc9de858c74
    Signed-off-by: klozz <klozz707@gmail.com>

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,6 +1,7 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2011-2018 The XPerience Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
