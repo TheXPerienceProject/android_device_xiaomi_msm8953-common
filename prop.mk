71c03971275723aa7dcb7026760e0fdca431debb
klozz <shaclone707@gmail.com>
2018-05-18 20:46:53 -0400

    msm8953-common: Trim properties extracted from addison OPN27.76-12-2 4
    Change-Id: I95b46a07dfd0097b3327881b2f2d7a1cb715f3c0
    Signed-off-by: klozz <shaclone707@gmail.com>

--- prop.mk
+++ prop.mk
@@ -134,6 +134,14 @@ ro.vendor.qti.sys.fw.bservice_enable=true \
 ro.vendor.qti.sys.fw.bservice_limit=5 \
 ro.vendor.qti.sys.fw.bservice_age=5000
 
+#Trim properties
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.vendor.qti.sys.fw.use_trim_settings=true \
+ro.vendor.qti.sys.fw.empty_app_percent=50 \
+ro.vendor.qti.sys.fw.trim_empty_percent=100 \
+ro.vendor.qti.sys.fw.trim_cache_percent=100 \
+ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
+
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.use_data_netmgrd=true \
