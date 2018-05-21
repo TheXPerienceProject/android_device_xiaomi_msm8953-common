4d457efa9c46602474bf8b7787a5c59030ea4f49
klozz <shaclone707@gmail.com>
2018-05-21 06:45:01 -0400

    msm8953-common: Sepolicy add sepolicy for android.hidl.manager and android.hidl.base due to these libs can't be mounted with enforced so allow them to be open in enforced to avoid random reboot due to GPS failing reading these libs :P
    Change-Id: Id7df89bd31a2a71ef101a68abc53bd7ba2b3125c
    Signed-off-by: klozz <shaclone707@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -113,6 +113,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    XPerienceCamera \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
