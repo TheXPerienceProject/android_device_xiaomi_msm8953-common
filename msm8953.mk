c1d049ac8a90831642777dde27791549afb88add
klozz <shaclone707@gmail.com>
2018-10-04 00:18:44 -0400

    msm8953-common: Update BT HALS
    Signed-off-by: klozz <shaclone707@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -114,6 +114,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    libbt-vendor \
     android.hardware.bluetooth@1.0-impl-qti
 
 # Camera
--- prop.mk
+++ prop.mk
@@ -53,6 +53,7 @@ persistent.bt.a2dp_offload_cap=sbc-aptx-aptXHD
 PRODUCT_PROPERTY_OVERRIDES += \
 bluetooth.hfp.client=1 \
 vendor.qcom.bluetooth.soc=smd \
+ro.qcom.bluetooth.soc=smd \
 ro.bluetooth.hfp.ver=1.7 \
 ro.bluetooth.sap=true  \
 ro.qualcomm.bluetooth.ftp=true \
