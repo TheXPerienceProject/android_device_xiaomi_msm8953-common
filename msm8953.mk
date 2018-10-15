dffdc59d8344f915ecac543d3cf1067aecd815e1
klozz <shaclone707@gmail.com>
2018-10-15 08:56:23 -0400

    msm8953-common: move to our old bt blobs
    Signed-off-by: klozz <shaclone707@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -114,7 +114,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0 \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # Camera
