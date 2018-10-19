5666ca8e23111a77ab182b3f9ece45b87e9dbbce
klozz <shaclone707@gmail.com>
2018-10-19 01:43:12 -0400

    drop miui cam
    Signed-off-by: klozz <shaclone707@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -128,8 +128,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
-$(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
-
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
