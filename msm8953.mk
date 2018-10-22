e6461464eb56b0ea0d43d7803071589e43179ff9
klozz <shaclone707@gmail.com>
2018-10-22 06:24:08 -0400

    msm8953-common: Drop libbt-vendor
    Signed-off-by: klozz <shaclone707@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -112,10 +112,6 @@ PRODUCT_COPY_FILES += \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-# Bluetooth HIDL
-PRODUCT_PACKAGES += \
-    libbt-vendor
-
 # Camera
 PRODUCT_PACKAGES += \
     Camera2 \
